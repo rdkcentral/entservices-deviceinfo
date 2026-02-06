5d5b1e58c12d62413e2fe688a090eca7aa261227
Preeja Raveendran <preeja_raveendran@comcast.com>
2026-02-06 12:31:53 +0530

    cherry-picked from develop

--- plugin/DeviceInfoImplementation.cpp
+++ plugin/DeviceInfoImplementation.cpp
@@ -156,16 +156,18 @@ namespace Plugin {
 
     Core::hresult DeviceInfoImplementation::Model(DeviceModel& deviceModel) const
     {
-        return
-#ifdef ENABLE_DEVICE_MANUFACTURER_INFO
-            (GetMFRData(mfrSERIALIZED_TYPE_PROVISIONED_MODELNAME, deviceModel.model) == Core::ERROR_NONE)
-            ? Core::ERROR_NONE
-            :
-#endif
-            GetFileRegex(_T("/etc/device.properties"),
-                std::regex("^FRIENDLY_ID(?:\\s*)=(?:\\s*)(?:\"{0,1})([^\"\\n]+)(?:\"{0,1})(?:\\s*)$"), deviceModel.model);
-    }
+        std::string device_name;
+        uint32_t result = GetFileRegex(_T("/etc/device.properties"), std::regex("^DEVICE_NAME(?:\\s*)=(?:\\s*)(?:\"{0,1})([^\"\\n]+)(?:\"{0,1})(?:\\s*)$"), device_name);
+        if ((result == Core::ERROR_NONE) && ((device_name == "PLATCO") || (device_name == "LLAMA"))) {
+            result = (GetMFRData(mfrSERIALIZED_TYPE_PROVISIONED_MODELNAME, deviceModel.model) == Core::ERROR_NONE) ? Core::ERROR_NONE
+		: GetFileRegex(_T("/etc/device.properties"), std::regex("^FRIENDLY_ID(?:\\s*)=(?:\\s*)(?:\"{0,1})([^\"\\n]+)(?:\"{0,1})(?:\\s*)$"), deviceModel.model);
+        } else {
+            result = GetFileRegex(_T("/etc/device.properties"), std::regex("^FRIENDLY_ID(?:\\s*)=(?:\\s*)(?:\"{0,1})([^\"\\n]+)(?:\"{0,1})(?:\\s*)$"), deviceModel.model);
+        }
 
+        return result;
+    }
+ 
     Core::hresult DeviceInfoImplementation::Brand(DeviceBrand& deviceBrand) const
     {
         deviceBrand.brand = "Unknown";
